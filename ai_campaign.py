678ebe5cf6300604e1f549b85dab2926e1aa406a
SrijaGaddam05 <gaddamsrija005@gmail.com>
2026-03-10 10:02:18 +0530

    Integrate FastAPI for marketing campaign services

--- ai_campaign.py
+++ ai_campaign.py
@@ -1,25 +1,77 @@
-from groq import Groq
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import UploadFile, File
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-client = Groq(
-    api_key="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"
+from ai_campaign import generate_campaign
+from sentiment import analyze_customer_feedback
+from trends import get_trends
+from sales_predictor import predict_sales
+from competitor_ai import analyze_competitor
+from strategy_ai import marketing_strategy
+from idea_ai import generate_ideas
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins during development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+@app.post("/upload-sales")
+async def upload_sales(file: UploadFile = File(...)):
 
-def generate_campaign(product, target):
+    df = pd.read_csv(file.file)
 
-    prompt = f"""
-    Create a marketing campaign for {product}.
-    Target audience: {target}.
-    Include:
-    - Tagline
-    - Social media campaign idea
-    - Hashtags
-    """
+    X = df[["month"]]
+    y = df["sales"]
 
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
+    model = LinearRegression()
+    model.fit(X,y)
 
-    return response.choices[0].message.content
+    prediction = model.predict([[12]])
+
+    return {"predicted_sales_month_12": float(prediction[0])}
+
+@app.get("/")
+def home():
+    return {"message": "MarketMind AI Platform Running"}
+
+@app.get("/campaign")
+def campaign(product: str, target: str):
+    result = generate_campaign(product, target)
+    return {"campaign": result}
+
+@app.get("/sentiment")
+def sentiment(text: str):
+    result = analyze_customer_feedback(text)
+    return {"sentiment": result}
+
+@app.get("/trends")
+def trends(keyword: str):
+    result = get_trends(keyword)
+    return {"trends": result}
+
+@app.get("/predict")
+def predict(month:int):
+    result = predict_sales(month)
+    return {"prediction":result}
+
+@app.get("/competitor")
+def competitor(company:str):
+    result = analyze_competitor(company)
+    return {"analysis":result}
+
+@app.get("/strategy")
+def strategy(product:str,audience:str):
+    result = marketing_strategy(product,audience)
+    return {"strategy":result}
+
+@app.get("/ideas")
+def ideas(product:str):
+    result = generate_ideas(product)
+    return {"ideas":result}
